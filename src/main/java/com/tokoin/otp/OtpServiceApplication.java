70a741427dd2bc242998f816ac4a1f6ca6c12b3a
rajithkonara <achini.meekandawattage@tokoin.io>
2019-12-19 16:21:33 +0530

    init adding the project

---
+++ src/main/java/com/tokoin/otp/OtpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tokoin.otp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtpServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tokoin/otp/OtpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokoin.otp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
