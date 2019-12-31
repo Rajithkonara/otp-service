8a5f51f821fab7e0778b84fa8560e1b04f88a51f
rajithkonara <rajith.konara@tokoin.io>
2019-12-31 12:35:46 +0530

    added general unit tests for controller

--- src/test/java/com/tokoin/otp/controller/OtpControllerTest.java
+++ src/test/java/com/tokoin/otp/controller/OtpControllerTest.java
@@ -2,58 +2,122 @@ package com.tokoin.otp.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tokoin.otp.dto.request.OtpRequestDto;
-import com.tokoin.otp.service.OtpService;
+import com.tokoin.otp.dto.request.OtpVerifyRequestDto;
+import com.tokoin.otp.service.OtpServiceImpl;
 import com.tokoin.otp.util.Validator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-
-import static org.mockito.MockitoAnnotations.initMocks;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * This class tests the  {@link OtpController}
  */
+@AutoConfigureMockMvc
 public class OtpControllerTest {
 
-    @Mock
-    private OtpService otpService;
-    private Validator validator = new Validator();
-    private OtpRequestDto otpRequestDto = emailOtpRequest();
+    private static final String INVALID_TYPE = "Invalid type provided";
+    private static final String OTP_GENERATE_URI = "/api/v1/otp/generate";
+    private static final String OTP_VERIFY_URI = "/api/v1/otp/verify";
 
-    @Autowired
+    @Mock
+    private OtpServiceImpl otpService;
+    private Validator validator = new Validator();
+    private OtpRequestDto otpRequestDtoEmail = otpRequestDto();
+    private OtpVerifyRequestDto otpVerifyRequestDto = otpVerifyRequestDto();
+
+    @InjectMocks
+    private OtpController otpController;
+
     private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        initMocks(this);
-        OtpController otpController = new OtpController(otpService, validator);
-        mockMvc = MockMvcBuilders.standaloneSetup(otpController).build();
+        MockitoAnnotations.initMocks(this);
+        otpController = new OtpController(otpService, validator);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(otpController).build();
     }
 
     @Test
-    void Should_ReturnOk_When_GenerateOppIsSuccessful() throws Exception {
+    void Should_ReturnOk_When_GenerateOtpIsSuccessful() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders
-                 .post("/api/v1/otp/generate")
+                .post(OTP_GENERATE_URI)
+                .contentType(MediaType.APPLICATION_JSON)
                 .header("app-key", "xxxx")
-                .content(asJsonString(otpRequestDto))
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .content(asJsonString(otpRequestDtoEmail)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void Should_ReturnOk_When_VerifyOtpIsSuccessful() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(OTP_VERIFY_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("app-key", "xxxx")
+                .content(asJsonString(otpVerifyRequestDto)))
                 .andExpect(status().isOk());
     }
 
 
-    private OtpRequestDto emailOtpRequest() {
+    @Test
+    void Should_ReturnError_When_InvalidTypeIsProvidedForGenerateOtp() throws Exception {
+
+        otpRequestDtoEmail.setType("text");
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(OTP_GENERATE_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("app-key", "xxxx")
+                .content(asJsonString(otpRequestDtoEmail)))
+                .andExpect(jsonPath("$.status", is("ERROR")))
+                .andExpect(jsonPath("$.message", is(INVALID_TYPE)))
+                .andExpect(jsonPath("$.data", is(nullValue())))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void Should_ReturnError_When_InvalidTypeIsProvidedForVerifyOtp() throws Exception {
+
+        otpVerifyRequestDto.setType("letter");
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(OTP_VERIFY_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("app-key", "xxxx")
+                .content(asJsonString(otpVerifyRequestDto)))
+                .andExpect(jsonPath("$.status", is("ERROR")))
+                .andExpect(jsonPath("$.message", is(INVALID_TYPE)))
+                .andExpect(jsonPath("$.data", is(nullValue())))
+                .andExpect(status().is4xxClientError());
+    }
+
+
+
+    private OtpRequestDto otpRequestDto() {
         OtpRequestDto otpRequestDto = new OtpRequestDto();
         otpRequestDto.setType("email");
         otpRequestDto.setEmail("randy@tokoin.io");
         return otpRequestDto;
+    }
+
+
+    private OtpVerifyRequestDto otpVerifyRequestDto() {
+        OtpVerifyRequestDto otpVerifyRequestDto = new OtpVerifyRequestDto();
+        otpVerifyRequestDto.setType("mobile");
+        otpVerifyRequestDto.setMobileNo("+6281123123");
+        otpVerifyRequestDto.setOtp("123456");
+        return otpVerifyRequestDto;
     }
 
     public static String asJsonString(final Object obj) {
